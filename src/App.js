5aff6d27da9052ba55666d5320d7a50a583d49ec
vanshitalavange <86543154+vanshitalavange@users.noreply.github.com>
2021-10-06 15:40:55 +0530

    feat : svg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
+import React from "react";
+import { useState } from "react";
+import calendar from "./images/calendar.svg";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Is it a Leap Year?</h1>
+        <div className="img-container">
+          <img id="hero-img" src={calendar} />
+        </div>
         <label>Enter a year:</label>
         <input
           onChange={(event) => setYear(event.target.value)}
