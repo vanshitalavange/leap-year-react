2d369505cfb942820825331f2a7f9e3dbdc3a631
vanshitalavange <86543154+vanshitalavange@users.noreply.github.com>
2021-10-06 15:37:32 +0530

    feat : basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Is it a Leap Year?</h1>
+        <label>Enter a year:</label>
+        <input
+          onChange={(event) => setYear(event.target.value)}
+          id="year"
+          type="number"
+        />
+        <button onClick={() => clickEventHandler()} id="btn-check">
+          check
+        </button>
+        <div id="output">{result}</div>
+      </div>
+    </div>
+  );
 }
